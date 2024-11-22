c8abccffb04f0987ab9b4a184d9e3953b8479191
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2024-11-22 18:44:03 -0300

    adaptadores sin uso comentados

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import vercelServerless from '@astrojs/vercel/serverless';
-import netlify from '@astrojs/netlify';
+// import vercelServerless from '@astrojs/vercel/serverless';
+// import netlify from '@astrojs/netlify';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
